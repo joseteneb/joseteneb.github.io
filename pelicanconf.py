4ee78bd5d4d087ac548936a036fe689f5942d474
Jose Teneb <jose@providentcrm.com>
2016-11-11 12:14:33 +0000

    add plugins to travis.yml

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-PLUGIN_PATHS = ['../pelican-plugins']
+PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['i18n_subsites', 'sitemap', 'pelican_gist']
 
 #PAGE_URL = '{slug}/'
