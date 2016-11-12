762aa53d0d4202f5df670adfbbec79d17747d137
Jose Teneb <joseteneb@gmail.com>
2016-11-12 20:03:15 +0000

    fixes

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,7 +33,7 @@ AUTHOR_FEED_RSS = None
 
 PLUGIN_PATHS = ['pelican-plugins']
 #PLUGIN_PATHS = ['../pelican-plugins']
-#PLUGINS = ['i18n_subsites', 'sitemap', 'pelican_gist']
+PLUGINS = ['i18n_subsites', 'sitemap', 'pelican_gist']
 
 #PAGE_URL = '{slug}/'
 #PAGE_SAVE_AS = '{slug}/index.html'
